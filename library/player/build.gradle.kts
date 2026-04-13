ab8b8a366d10ffae3ae04287f5b723bc6042eafa
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2026-04-13 07:13:03 +0200

    feat: add player library module and configuration

---
+++ library/player/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    kotlin("android")
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "soy.gabimoreno.player"
+    compileSdk =
+        libs.versions.sdk.compile
+            .get()
+            .toInt()
+
+    defaultConfig {
+        minSdk =
+            libs.versions.sdk.minimum
+                .get()
+                .toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_21.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.google.material)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.compose.ui)
+    implementation(libs.palette)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.kluent.android)
+    testImplementation(libs.mockk)
+    testImplementation(libs.robolectric)
+
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.mockk.android)
+}
